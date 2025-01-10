41d03a3144c4949185c6431ffac91fd94ae9ffb1
Jay Harris <jay.harris@outlook.co.nz>
2025-01-10 15:11:53 +1300

    [AI Chat]: Add UI for Tab Informer

--- components/ai_chat/resources/page/state/conversation_context.tsx
+++ components/ai_chat/resources/page/state/conversation_context.tsx
@@ -59,6 +59,9 @@ export type ConversationContext = SendFeedbackState & CharCountContext & {
   setIsToolsMenuOpen: (isOpen: boolean) => void
   handleVoiceRecognition?: () => void
   conversationHandler?: Mojom.ConversationHandlerRemote
+
+  showAttachments: boolean
+  setShowAttachments: (show: boolean) => void
 }
 
 export const defaultCharCountContext: CharCountContext = {
@@ -96,6 +99,8 @@ const defaultContext: ConversationContext = {
   resetSelectedActionType: () => { },
   handleActionTypeClick: () => { },
   setIsToolsMenuOpen: () => { },
+  showAttachments: false,
+  setShowAttachments: () => { },
   ...defaultSendFeedbackState,
   ...defaultCharCountContext
 }
@@ -153,7 +158,15 @@ export const ConversationReactContext =
 
 export function ConversationContextProvider(props: React.PropsWithChildren) {
   const [context, setContext] =
-    React.useState<ConversationContext>(defaultContext)
+    React.useState<ConversationContext>({
+      ...defaultContext,
+      setShowAttachments: (showAttachments: boolean) => {
+        setContext((value) => ({
+          ...value,
+          showAttachments
+        }))
+      }
+    })
 
   const aiChatContext = useAIChat()
   const { conversationHandler, callbackRouter, selectedConversationId, updateSelectedConversationId } = useActiveChat()
@@ -170,6 +183,10 @@ export function ConversationContextProvider(props: React.PropsWithChildren) {
       ...partialContext
     }))
   }
+
+  React.useEffect(() => {
+    context.setShowAttachments(!!aiChatContext.isStandalone && !aiChatContext.visibleConversations.some(c => c.uuid === context.conversationUuid))
+  }, [context.conversationUuid])
 
   const getModelContext = (
     currentModelKey: string,
@@ -521,4 +538,11 @@ export function ConversationContextProvider(props: React.PropsWithChildren) {
 
 export function useConversation() {
   return React.useContext(ConversationReactContext)
+}
+
+export function useSupportsAttachments() {
+  const aiChatContext = useAIChat()
+  const conversationContext = useConversation()
+  return aiChatContext.isStandalone
+    && !aiChatContext.visibleConversations.find(c => c.uuid === conversationContext.conversationUuid)
 }
