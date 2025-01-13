b28fcb85f3ae8636ba77f4839ffd324cac4e470e
Claudio DeSouza <cdesouza@brave.com>
2025-01-13 12:38:41 +0000

    [cr134] Use `GetTabSearchButton`
    All uses of `GetTabSearchContainer` in Brave are in place to retrieve
    the tab search button. Chromium has simplified this access by providing
    `GetTabSearchButton()`, to allow access directly to it.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/ceb8a5639af61500b90153c391a1f4f8a3eeb6e9
    
    commit ceb8a5639af61500b90153c391a1f4f8a3eeb6e9
    Author: Emily Shack <emshack@chromium.org>
    Date:   Thu Jan 9 15:19:55 2025 -0800
    
        Remove nudges from combo button
    
        The nudges have now been moved to TabStripActionContainer when the combo
        button is enabled, so the combo button should never include the nudges
        itself. This swaps out the tab search container for a tab search button,
        as built by the combo button.
    
        Bug: 384099721

--- browser/ui/views/frame/brave_browser_view.cc
+++ browser/ui/views/frame/brave_browser_view.cc
@@ -587,13 +587,10 @@ void BraveBrowserView::UpdateSecondaryDevtoolsLayoutAndVisibility(
 }
 
 void BraveBrowserView::UpdateSearchTabsButtonState() {
-  if (auto* tab_search_container =
-          tab_strip_region_view()->GetTabSearchContainer()) {
-    if (auto* button = tab_search_container->tab_search_button()) {
-      auto is_tab_search_visible =
-          GetProfile()->GetPrefs()->GetBoolean(kTabsSearchShow);
-      button->SetVisible(is_tab_search_visible);
-    }
+  if (auto* tab_search_button = tab_strip_region_view_->GetTabSearchButton()) {
+    auto is_tab_search_visible =
+        GetProfile()->GetPrefs()->GetBoolean(kTabsSearchShow);
+    tab_search_button->SetVisible(is_tab_search_visible);
   }
 }
 
