a2ede93a7c433402655ebafb10bff35432b4bea0
Jay Harris <jay.harris@outlook.co.nz>
2025-01-16 09:45:01 +1300

    Maybe fix ios build?

--- ios/browser/api/ai_chat/conversation_client.mm
+++ ios/browser/api/ai_chat/conversation_client.mm
@@ -71,7 +71,7 @@ void ConversationClient::OnSuggestedQuestionsChanged(
 void ConversationClient::OnAssociatedContentInfoChanged(
     const mojom::AssociatedContentPtr site_info,
     bool should_send_content) {
-  [bridge_ onPageHasContent:[[AiChatSiteInfo alloc]
+  [bridge_ onPageHasContent:[[AiChatAssociatedContent alloc]
                                 initWithSiteInfoPtr:site_info->Clone()]
           shouldSendContent:should_send_content];
 }
