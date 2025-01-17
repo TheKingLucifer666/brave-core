36292c72615a2fbfc09a9328249f311b12559a7b
Serg <serg.zhukovsky@gmail.com>
2025-01-17 16:32:21 -0500

    Autofocuses input box on Android Leo chat window on a fresh chat

--- components/ai_chat/resources/page/components/main/index.tsx
+++ components/ai_chat/resources/page/components/main/index.tsx
@@ -205,6 +205,17 @@ function Main() {
     }
   }
 
+  const maybeShowSoftKeyboard = (querySubmitted: boolean) => {
+    if (aiChatContext.isMobile && aiChatContext.hasAcceptedAgreement &&
+      conversationContext.historyInitialized && !querySubmitted &&
+      !conversationContext.isGenerating &&
+      conversationContext.conversationHistory.length === 0) {
+        aiChatContext.uiHandler?.handleShowSoftKeyboard()
+        return true
+    }
+    return false
+  }
+
   return (
     <main className={styles.main}>
       {isConversationListOpen && !aiChatContext.isStandalone && (
@@ -350,6 +361,7 @@ function Main() {
           <InputBox
             context={{ ...conversationContext, ...aiChatContext }}
             onFocusInputMobile={handleOnFocusInputMobile}
+            maybeShowSoftKeyboard={maybeShowSoftKeyboard}
           />
         </ToolsButtonMenu>
       </div>
